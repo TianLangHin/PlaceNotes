502fbd45f24bab97af66fd11195ab5b53f18ec0f
TianLangHin <tianlanghin@gmail.com>
2025-09-20 12:19:27 +1000

    Initial Commit

---
+++ PlaceNotes/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PlaceNotes
+//
+//  Created by Tian Lang Hin on 20/9/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PlaceNotes/PlaceNotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlaceNotesApp.swift
+//  PlaceNotes
+//
+//  Created by Tian Lang Hin on 20/9/2025.
+//
+
+import SwiftUI
+
+@main
+struct PlaceNotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
